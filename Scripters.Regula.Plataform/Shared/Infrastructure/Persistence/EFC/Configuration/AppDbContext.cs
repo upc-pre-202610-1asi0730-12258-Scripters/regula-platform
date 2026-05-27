b73b9926729f4af5a1f71478e85f30b12797e536
Leonardo Lopez <leonardolopezsegundob@gmail.com>
2026-05-27 05:37:21 -0500

    feat: implement AppDbContext with snake_case naming convention and string extensions

---
+++ Scripters.Regula.Plataform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Scripters.Regula.Plataform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using Scripters.Regula.Plataform.Shared.Infrastructure.Persistence.EFC.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scripters.Regula.Plataform.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddInterceptors(new AuditableEntityInterceptor());
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // TODO: Register entities here
+
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
