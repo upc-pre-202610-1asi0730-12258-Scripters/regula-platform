d3b805f960dfe151c376f10eb03e72888342ba99
eliocerdan <eliasdanielramoscerdan@gmail.com>
2026-06-10 03:24:31 -0500

    feat(commercial-management): implement create customer debt command service

--- Scripters.Regula.Platform/CommercialManagement/Application/CommandServices/ICustomerDebtCommandService.cs
+++ Scripters.Regula.Platform/CommercialManagement/Application/CommandServices/ICustomerDebtCommandService.cs
@@ -1,6 +1,10 @@
+using Scripters.Regula.Platform.CommercialManagement.Domain.Model.Commands;
+using Scripters.Regula.Platform.CommercialManagement.Domain.Model.Entities;
+using Scripters.Regula.Platform.Shared.Application.Model;
+
 namespace Scripters.Regula.Platform.CommercialManagement.Application.CommandServices;
 
-public class ICustomerDebtCommandService
+public interface ICustomerDebtCommandService
 {
-    
+    Task<Result<CommercialDebt>> Handle(CreateCustomerDebtCommand command, CancellationToken cancellationToken = default);
 }
