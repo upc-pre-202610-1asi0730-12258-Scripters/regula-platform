7cb96e67ee63e24bfb1eb0880fc82468688b53fd
Alumnos <alumnos@A509-05.local>
2026-06-10 15:06:48 -0500

    feat(commercial-management): register customer debt dependendcies

--- Scripters.Regula.Platform/Program.cs
+++ Scripters.Regula.Platform/Program.cs
@@ -1,17 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Scripters.Regula.Platform.CommercialManagement.Application.CommandServices;
+using Scripters.Regula.Platform.CommercialManagement.Application.Internal.CommandServices;
+using Scripters.Regula.Platform.CommercialManagement.Domain.Repositories;
+using Scripters.Regula.Platform.CommercialManagement.Infrastructure.Persistence.EFC.Repositories;
+using Scripters.Regula.Platform.Shared.Domain.Repositories;
+using Scripters.Regula.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Scripters.Regula.Platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Configure Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (connectionString != null) options.UseMySQL(connectionString);
+});
+
+// Configure Dependency Injection
+
+// Commercial Management Bounded Context
+builder.Services.AddScoped<ICommercialCustomerRepository, CommercialCustomerRepository>();
+builder.Services.AddScoped<ICommercialDebtRepository, CommercialDebtRepository>();
+builder.Services.AddScoped<ICustomerDebtCommandService, CustomerDebtCommandService>();
+
+// Shared Bounded Context
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +47,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.Run();
