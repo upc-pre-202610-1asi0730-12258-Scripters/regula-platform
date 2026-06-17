acf33cdf87714a1d83f33e4453cdc86fbb91b204
eliocerdan <eliasdanielramoscerdan@gmail.com>
2026-06-16 19:53:36 -0500

    fix(database): keep migrations on startup

--- Scripters.Regula.Platform/Program.cs
+++ Scripters.Regula.Platform/Program.cs
@@ -101,4 +101,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
 app.Run();
