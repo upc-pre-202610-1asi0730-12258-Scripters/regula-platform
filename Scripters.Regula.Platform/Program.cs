fb4f60abde7e34651178cce20bea7151f6f742e6
Kevin <lopezmontalvokevin2021@upc.edu.pe>
2026-06-17 14:13:38 -0500

    fix: removed developtment conditional for swagger deployment

--- Scripters.Regula.Platform/Program.cs
+++ Scripters.Regula.Platform/Program.cs
@@ -99,12 +99,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
